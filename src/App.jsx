eb51762416df53c533ed08baeaddb7db3fa9268f
Piyush Gupta <piyushgupta941d@gmail.com>
2024-01-04 00:56:43 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/no-unknown-property */
+import { useEffect, useRef, useMemo } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera, useHelper } from '@react-three/drei'
+import * as THREE from 'three'
+import useKeyboard from './helpers/useKeyboard'
+
+function App() {
+
+  const Plane = () => {
+    return (
+      <mesh rotation={[Math.PI/2, 0, 0]}>
+        <planeGeometry args={[5, 5]} />
+        <meshBasicMaterial color="hotpink" side={THREE.DoubleSide} />
+      </mesh>
+    )
+  }
+
+  const Pov = () => {
+    const povRef = useRef()
+    useHelper(povRef, THREE.CameraHelper)
+    const keyMap = useKeyboard()
+
+    useFrame((_, delta) => {
+      keyMap['KeyA'] && (povRef.current.position.x -= 1 * delta)
+      keyMap['KeyD'] && (povRef.current.position.x += 1 * delta)
+      keyMap['KeyW'] && (povRef.current.position.z -= 1 * delta)
+      keyMap['KeyS'] && (povRef.current.position.z += 1 * delta)
+    })
+
+    return (
+      <PerspectiveCamera ref={povRef} position={[0, 0.07, 2]} rotation={[-0.15, 0, 0]} makeDefault  />
+    )
+  }
+
+
+  return (
+    <div className='h-screen w-screen'>
+      <Canvas camera={{position: [0, 0.5, 0.3]}}>
+        <Plane />
+          <Pov />
+        {/* <OrbitControls /> */}
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
