3efe596186216668d1f8b92aee05adbb67c422bb
Piyush Gupta <piyushgupta941d@gmail.com>
2024-01-06 01:47:09 +0530

    mapping all initial players

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,17 @@ import { connectSocket, sendModel } from './helpers/socketConnection'
 
 function App() {
 
+  const [loading, setLoading] = useState(false)
+  const [players, setPlayers] = useState(null)
+  const [playerKeys, setPlayerKeys] = useState(null)
+
   const socket = useMemo(() => connectSocket(), []);
-  sendModel(socket, {position: {x: 0, y: 0.2, z: 2}, rotation: {x: 0, y: 0, z: 0}})
+  sendModel(socket, {position: {x: 0, y: 0.2, z: 2}, rotation: {_x: 0, _y: 0, _z: 0}})
   
+  useEffect(() => {
+    getPlayers()
+  }, [])
+
   const Plane = () => {
     return (
       <mesh rotation={[Math.PI/2, 0, 0]}>
@@ -49,16 +57,48 @@ function App() {
     )
   }
 
+  const getPlayers = () => {
+    setLoading(true)
+    socket.on('get-all-users', (players) => {
+      setPlayers(players)
+      const keys = Object.keys(players)
+      setPlayerKeys(keys)
+    })
+    setLoading(false)
+  }
+
+  const Player = (value) => {
+    return (
+      <mesh position={[value.position.x, value.position.y, value.position.z]} rotation={[value.rotation._x, value.rotation._y, value.rotation._z]}>
+        <boxGeometry args={[0.1, 0.1, 0.1]} />
+        <meshBasicMaterial color='red' />
+      </mesh>
+    )
+  }
+
 
   return (
     <div className='h-screen w-screen'>
-      <Canvas camera={{position: [0, 0.5, 0.3]}}>
-        <Plane />
-        <Pov />
-        <gridHelper />
-        {/* <OrbitControls /> */}
-        <Stats />
-      </Canvas>
+      {
+        !loading ?
+        <Canvas camera={{position: [0, 0.5, 0.3]}}>
+          <Plane />
+          <Pov />
+          {
+            playerKeys &&
+            playerKeys.map((key, index) => {
+              return (
+                <Player key={index} position={players[key].position} rotation={players[key].rotation} />
+              )
+            })
+          }
+          <gridHelper />
+          {/* <OrbitControls /> */}
+          <Stats />
+        </Canvas>
+        :
+        <h1>Loading...</h1>
+      }
     </div>
   )
 }
