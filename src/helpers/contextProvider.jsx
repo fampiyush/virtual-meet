94d9bc4d95cd74cc6a354351abbf4428f0944bd9
Piyush Gupta <piyushgupta941d@gmail.com>
2024-01-19 01:28:08 +0530

    added myName state in context

--- src/helpers/contextProvider.jsx
+++ src/helpers/contextProvider.jsx
@@ -5,10 +5,11 @@ const PlayerContext = createContext([]);
 
 // Create a provider component
 const ContextProvider = ({ children }) => {
-    const [playerKeys, setPlayerKeys] = useState([])
+    const [playerKeys, setPlayerKeys] = useState([]);
+    const [myName, setMyName] = useState('');
 
     return (
-        <PlayerContext.Provider value={[ playerKeys, setPlayerKeys ]}>
+        <PlayerContext.Provider value={{ playerKeys, setPlayerKeys, myName, setMyName }}>
             {children}
         </PlayerContext.Provider>
     );
