25e88360e6dd3606195e6ac1f2cd74cb99b05c91
Piyush Gupta <piyushgupta941d@gmail.com>
2024-01-26 21:01:34 +0530

    added peerConn in context

--- src/helpers/contextProvider.jsx
+++ src/helpers/contextProvider.jsx
@@ -7,9 +7,10 @@ const PlayerContext = createContext([]);
 const ContextProvider = ({ children }) => {
     const [playerKeys, setPlayerKeys] = useState([]);
     const [myName, setMyName] = useState('');
+    const [peerConn, setPeerConn] = useState([]);
 
     return (
-        <PlayerContext.Provider value={{ playerKeys, setPlayerKeys, myName, setMyName }}>
+        <PlayerContext.Provider value={{ playerKeys, setPlayerKeys, myName, setMyName, peerConn, setPeerConn }}>
             {children}
         </PlayerContext.Provider>
     );
