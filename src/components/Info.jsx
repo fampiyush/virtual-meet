4e10f15b8cdaad1bca3319c35f38f59ff6d65879
Piyush Gupta <piyushgupta941d@gmail.com>
2024-02-02 16:00:07 +0530

    changes in css info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,7 @@
 import React,{ useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { IoMdInformationCircleOutline } from "react-icons/io"
-import { MdOutlineContentCopy } from "react-icons/md"
+import { MdOutlineContentCopy, MdLock } from "react-icons/md"
 import { IoClose } from "react-icons/io5"
 
 const Info = () => {
@@ -40,23 +40,23 @@ const Info = () => {
                 <IoMdInformationCircleOutline size={30} color='#5c89d1' />
             </button>
             :
-            <div className='fixed top-1 left-1 z-20 2xl:max-w-[15%] xl:max-w-[20%] lg:max-w-[20%] md:max-w-[30%] max-w-[50%] p-2 bg-[#5c89d1] rounded flex-col text-center'>
+            <div className='fixed top-1 left-1 z-20 2xl:max-w-[20%] xl:max-w-[25%] lg:max-w-[30%] md:max-w-[35%] max-w-[55%] p-2 bg-[#5c89d1] rounded flex-col text-center'>
                 <div className='absolute right-1 top-1'>
                     <button className='active:opacity-50' onClick={() => setShowModal(false)}>
                         <IoClose size={25} color='#fff' />
                     </button>
                 </div>
                 <h1 className='text-lg'>Virtual Meet</h1>
-                <div className='grid grid-rows-4 grid-cols-2 mt-4 gap-2'>
+                <div className='grid grid-rows-3 grid-cols-2 mt-4 gap-2'>
                     <div className='max-w-[fit-content]'>Meeting Id: </div>
-                    <div className='flex'>
-                        <span className='bg-[#575857] rounded px-1 mr-1'>{meetingId}</span>
+                    <div className='flex bg-[#575857] max-w-[fit-content] p-1 rounded'>
+                        <span className='rounded px-1 mr-1'>{meetingId}</span>
                         <button className='mt-[0.05rem] active:opacity-50' onClick={copyId}>
                             <MdOutlineContentCopy size={20} color='#fff' />
                         </button>
                     </div>
                     <div className='max-w-[fit-content]'>Meeting Link:</div>
-                    <div className='row-span-3 break-words'>
+                    <div className='row-span-2 break-words'>
                         <span>{baseUrl}{meetingId}</span>
                         <button className='text-sm flex text-center mt-2 max-w-[fit-content] bg-[#dfdcdc] rounded px-1 active:opacity-50' onClick={copyLink}>
                             <span className='mr-1 text-[#0000EE]'>Copy Link</span>
@@ -65,6 +65,10 @@ const Info = () => {
                             </span>
                         </button>
                     </div>
+                </div>
+                <div className='text-xs mt-4 flex justify-center text-center'>
+                    <span>This Meeting is end-to-end encrypted.</span>
+                    <span className='ml-1 mt-[0.1rem]'><MdLock color='#39fa73' /></span>
                 </div>
             </div>
         }
