7244ef6a99e4cfe1200a5334b3b0484995663b5f
Piyush Gupta <piyushgupta941d@gmail.com>
2024-02-05 18:07:40 +0530

    own video prototype

---
+++ src/components/OwnVideo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OwnVideo = () => {
+  return (
+    <div className='fixed bottom-0 left-0 z-20'>
+        <img src='/placeholder.jpg' className='h-44 w-52' />
+    </div>
+  )
+}
+
+export default OwnVideo
