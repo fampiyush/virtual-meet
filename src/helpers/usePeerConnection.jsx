e967b3c33dc471dc3db6e7861ea328a94de287b5
Piyush Gupta <piyushgupta941d@gmail.com>
2025-12-30 12:53:35 +0530

    Fixes Peer config

--- src/helpers/usePeerConnection.jsx
+++ src/helpers/usePeerConnection.jsx
@@ -22,7 +22,6 @@ const usePeerConnection = (getMedia, setLoading, triggerMessagePopup) => {
       const peerConnection = new Peer({
         host: import.meta.env.VITE_PEER_HOST,
         path: '/peer',
-        port: 3000,
         secure: true,
       });
       peerConnection.on("open", () => {
